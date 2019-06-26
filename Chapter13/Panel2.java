4dd5609a07933e6387d87a4b0ed0ac6562a3ec3d
Artemii Petrovskii <artemii.petrovskii@outlook.com>
2019-06-26 16:17:17 +0300

    modified

---
+++ Chapter13/Panel2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel2 {
+   public static void main(String[] args) {
+	   Panel2 gui = new Panel2();
+	   gui.go();
+   }
+   public void go() {
+	   JFrame frame = new JFrame();
+	   JPanel panel = new JPanel();
+	   panel.setBackground(Color.darkGray);
+	   JButton button = new JButton("shock me");
+	   panel.add(button);
+	   frame.getContentPane().add(BorderLayout.EAST, panel);
+	   frame.setSize(200,200);
+	   frame.setVisible(true);
+   }
+}
