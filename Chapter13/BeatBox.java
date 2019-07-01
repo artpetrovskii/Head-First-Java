54bb57dc4354ee6851f32864112036c0411dfbdc
Artemii Petrovskii <artemii.petrovskii@outlook.com>
2019-07-01 19:28:44 +0300

    modified

--- Chapter13/BeatBox.java
+++ Chapter13/BeatBox.java
@@ -154,6 +154,63 @@ public class BeatBox {
             } catch(Exception ex) {
                ex.printStackTrace();
             }
+        }
+    }
+    public class MyReadInListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            boolean[] checkboxState = null;
+            try {
+                FileInputStream fileIn = new FileInputStream(new File("CheckBox.ser"));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[]) is.readObject();
+            } catch(Exception ex) {ex.printStackTrace();}
+            for (int i = 0; i < 256; i++) {
+              JCheckBox check = (JCheckBox) checkboxList.get(i);
+              if (checkboxState[i]) {
+                 check.setSelected(true);
+              } else {
+                 check.setSelected(false);
+               }
+            }
+            sequencer.stop();
+            buildTrackAndStart();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
